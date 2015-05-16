fc33d4cb82cfcdc355ad5716b7a70455e23d0400
Francisco García Díaz <pakiyo_pgd@hotmail.com>
2015-05-16 20:27:29 +0200

    Grid added

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -3,9 +3,18 @@ using System.Collections;
 
 public class Grid : MonoBehaviour {
 
-	private Square board;
+	private float altitude = 0.1f;
+	private Square [][]board; //Board of the game
+	private int xBoardSize = 20; //Number of boxes in X
+	private int yBoardSize = 20; //Number of boxes in Y
+	private int boxSize = 10; //Size of one box
+
+	int i,j;
 
 	void Start(){
+		for (i=0; i<xBoardSize; i++)
+			for (j=0; j<yBoardSize; j++)
+				board [i] [j] = new Square(i,j);
 
 	}
 
@@ -14,7 +23,24 @@ public class Grid : MonoBehaviour {
 	}
 
 	void OnPostRender(){
-		board.OnPostRender ();
+		for (i=0; i<xBoardSize; i++){
+			for (j=0; j<yBoardSize; j++){
+				int xPos = i*boxSize;
+				int zPos = j*boxSize;
+
+				GL.Begin(GL.LINES);
+				GL.Color(Color.red);
+				GL.Vertex3(xPos, altitude, zPos);
+				GL.Vertex3(xPos+boxSize, altitude, zPos);
+				GL.Vertex3(xPos+boxSize, altitude, zPos);
+				GL.Vertex3(xPos+boxSize, altitude, zPos+boxSize);
+				GL.Vertex3(xPos+boxSize, altitude, zPos+boxSize);
+				GL.Vertex3(xPos, altitude, zPos+boxSize);
+				GL.Vertex3(xPos, altitude, zPos+boxSize);
+				GL.Vertex3(xPos, altitude, zPos);
+				GL.End();
+			}
+		}
 	}
 
 }
--- Assets/Scripts/Square.cs
+++ Assets/Scripts/Square.cs
@@ -6,29 +6,22 @@ public class Square : MonoBehaviour {
 	private int posX;
 	private int posY;
 
-	private float altitude = 0.1f;
-
-	// Use this for initialization
-	void Start (int X, int Y) {
+	public Square(int X, int Y){
 		posX = X;
 		posY = Y;
+	}
+
+	// Use this for initialization
+	void Start () {
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	}
 
-	public void OnPostRender(){
-		GL.Begin(GL.LINES);
-		GL.Color(Color.red);
-		GL.Vertex3(0, altitude, 0);
-		GL.Vertex3(10, altitude, 0);
-		GL.Vertex3(10, altitude, 0);
-		GL.Vertex3(10, altitude, 10);
-		GL.Vertex3(10, altitude, 10);
-		GL.Vertex3(0, altitude, 10);
-		GL.Vertex3(0, altitude, 10);
-		GL.Vertex3(0, altitude, 0);
-		GL.End();
 	}
+	
+
+
+
 }
