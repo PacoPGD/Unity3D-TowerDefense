a99ff31661e345bf3043d0b441400304bc8cfe99
Francisco García Díaz <pakiyo_pgd@hotmail.com>
2015-05-23 15:57:25 +0200

    Backup of algorithm

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 
 
 public class Enemy : MonoBehaviour {
@@ -15,20 +15,20 @@ public class Enemy : MonoBehaviour {
 
 	private static int GOAL = 0;
 	private static int FREE = 1;
-	//private static int CHECKED= 2;
-	//private static int FINALIZED= 3;
-	private static int BLOCK= 4;
+	private static int CHECKED= 2;
+	private static int BLOCK= 3;
 
 	private int x; //X coordinate in the board of this enemy
 	private int z; //Z coordinate in the board of this enemy
 
-	private Node myNode;
+	private Node [,] myNode;
 
 	// Use this for initialization
 	void Start () {
 		life = maxLife;
-		myNode = new Node();
+
 		myBoardStatus = new int[gridStatus.xSize, gridStatus.zSize];
+		myNode = new Node[gridStatus.xSize, gridStatus.zSize];
 		loadBoardStatus ();
 		routeCalculation ();
 
@@ -40,13 +40,52 @@ public class Enemy : MonoBehaviour {
 	}
 
 	public void routeCalculation(){
-		myNode.x = x;
-		myNode.z = z;
+		List<Node> tail = new List<Node>();
+		Node checking;
 
-		myNode.findNeighbors (myBoardStatus);
+		for (int i =0; i<gridStatus.xSize; i++) {
+			for(int j=0; j<gridStatus.zSize;j++){
+				myNode[i,j] = new Node(FREE,i,j);
+			}
+		}
+
+		//Add the father element 
+		tail.Add (myNode [x, z]);
+		myNode [x, z].myStatus = CHECKED;
+
+		while (tail.Count!=0) {
+			checking = tail[0];
+			tail.RemoveAt (0);
+			//Debug.Log ("Checking "+ (checking.x) +" "+(checking.z));
+
+			for(int i=-1;i<=1;i++){
+				for(int j=-1;j<=1;j++){
+					if(inBoard (checking.x+i,checking.z+j)){
+						if(myNode[checking.x+i,checking.z+j].myStatus==FREE){
+							myNode[checking.x+i,checking.z+j].myStatus=CHECKED;
+							myNode[checking.x+i,checking.z+j].distance=checking.distance+1;
+							myNode[checking.x+i,checking.z+j].previous = checking;
+							tail.Add (myNode[checking.x+i,checking.z+j]);
+							
+							//Debug.Log ("Add "+ (checking.x+i) +" "+(checking.z+j));
+						}
+					}
+				}
+			}
+					
+		}
+
 
 	}
 	
+
+	public bool inBoard(int x, int z){
+		if (z >= 0 && z < gridStatus.zSize && x >= 0 && x < gridStatus.xSize) 
+			return true;
+	    else
+			return false;
+	}
+
 
 	public void goSquare(int squareX, int squareZ){
 		if (x < squareX)
@@ -73,13 +112,7 @@ public class Enemy : MonoBehaviour {
 		}
 	}
 
-	public void logBoardStatus(){
-		for (int i=0; i<gridStatus.xSize; i++) {
-			for(int j=0; j<gridStatus.zSize; j++){
-				Debug.Log ("posicion"+i+" "+j+" "+myBoardStatus[i,j]);
-			}
-		}
-	}
+
 
 	public void setX(int value){
 		x = value;
--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -1,106 +1,23 @@
 using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 
 public class Node {
+	private static int INFINITE = 999;
+	//private static int GOAL = 0;
+	private static int FREE = 1;
+	//private static int CHECKED= 2;
 
-	
-	public int [,] myBoardStatus;
+	public int x;
+	public int z;
 
-	public int x; //X coordinate in the board of this enemy
-	public int z; //Z coordinate in the board of this enemy
-	public List<Node> next = new List<Node>(); //List of next nodes
+	public int myStatus = FREE;
+	public int distance = INFINITE;
 	public Node previous=null;
 
-	private static int GOAL = 0;
-	private static int FREE = 1;
-	private static int CHECKED= 2;
-	private static int FINALIZED= 3;
-	//private static int BLOCK= 4;
-
-	public Node(){
-	}
-
-	public Node(int valueX, int valueZ){
+	public Node(int status, int valueX, int valueZ){
+		myStatus = status;
 		x = valueX;
 		z = valueZ;
-	}
-
-	public void findNeighbors(int [,] value){
-		myBoardStatus = value;
-
-		if (getNodeStatus () == GOAL) {
-			Debug.Log ("success");
-		} 
-
-		else {
-			if(previous != null && getNodeStatus()==FINALIZED){
-
-			}
-			else{
-				setNodeStatus (CHECKED);
-				Debug.Log ("check " + (x) + " " + (z));
-				if(previous==null){
-					if(!addNextNode ()){
-						Debug.Log ("finish " + (x) + " " + (z));
-						setNodeStatus (FINALIZED);
-						for (int i=0; i<next.Count; i++) {
-							next [i].findNeighbors (myBoardStatus);
-						}	
-					}
-				}
-				else{
-					if (previous.getNodeStatus () != FINALIZED) {
-						previous.findNeighbors (myBoardStatus);
-					} 
-					else {
-						if(!addNextNode ()){
-							Debug.Log ("finish " + (x) + " " + (z));
-							setNodeStatus (FINALIZED);
-							for (int i=0; i<next.Count; i++) {
-								next [i].findNeighbors (myBoardStatus);
-							}	
-						}
-					}
-				}
-			}
-
-		}
-	}
-
-	public bool addNextNode(){
-		Node aux;
-		for(int i=-1;i<=1;i++){
-			for(int j=-1;j<=1;j++){
-				if(inBoard (x+i,z+j)){
-					if (myBoardStatus [x+i,z+j] == FREE) {
-						aux = new Node (x+i, z+j);
-						next.Add (aux);
-						aux.previous = this;
-						aux.findNeighbors (myBoardStatus);
-						return true;
-					}
-				}
-			}
-		}
-
-		return false;
-	}
-	
-	public void setNodeStatus(int value){
-		myBoardStatus[x,z] = value;
-	}
-
-	public int getNodeStatus(){
-		return myBoardStatus[x,z];
-	}
-
-	public bool inBoard(int x, int z){
-		if (z  >= 0 && z  < gridStatus.zSize && x >= 0 && x  < gridStatus.xSize) {
-			return true;
-		} else {
-			return false;
-		}
 	}
 
 }
