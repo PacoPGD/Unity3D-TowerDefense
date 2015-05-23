d02c5c3697457f65dc470e63b29a804416775593
Francisco García Díaz <pakiyo_pgd@hotmail.com>
2015-05-23 18:28:09 +0200

    reverb

--- Assets/Scripts/Square.cs
+++ Assets/Scripts/Square.cs
@@ -15,18 +15,24 @@ public class Square : MonoBehaviour {
 	public GameObject blueCannonTurret; // blueCannonTurret GameObject, represent a Blue Cannon Turret
 	public GameObject enemyNormal; // enemyNormal GameObject, represent a Normal Enemy
 
+<<<<<<< HEAD
+=======
+	private bool isBuildable = true; //It indicates if the square is buildable
+	private Status myStatus=Status.Free;
+
+>>>>>>> parent of 8ca0939... Static boardStatus added
 	private int x;
 	private int z;
-	
+
 	void Start(){
-		if(gridStatus.myStatus[x,z]!=gridStatus.Status.Generator)
+		if(myStatus!=Status.Generator)
 			gameObject.GetComponent<Renderer>().material.color = Color.white;
 		else
 			gameObject.GetComponent<Renderer>().material.color = Color.red;
 	}
 
 	void OnMouseDown() {
-		if (gridStatus.myStatus[x,z] == gridStatus.Status.Free) {
+		if (myStatus == Status.Free) {
 			blueCannonTurret = (GameObject)Instantiate (blueCannonTurret);
 			blueCannonTurret.transform.position += transform.position;
 			gridStatus.myStatus[x,z] = gridStatus.Status.Tower;
@@ -36,14 +42,14 @@ public class Square : MonoBehaviour {
 
 
 	void OnMouseOver() {
-		if(gridStatus.myStatus[x,z]==gridStatus.Status.Free)
+		if(myStatus==Status.Free)
 			gameObject.GetComponent<Renderer>().material.color = Color.blue;
 		else
 			gameObject.GetComponent<Renderer>().material.color = Color.red;
 	}
 
 	void OnMouseExit() {
-		if(gridStatus.myStatus[x,z]==gridStatus.Status.Generator)
+		if(myStatus==Status.Generator)
 			gameObject.GetComponent<Renderer>().material.color = Color.red;
 		else
 			gameObject.GetComponent<Renderer>().material.color = Color.white;
@@ -52,7 +58,7 @@ public class Square : MonoBehaviour {
 
 
 	public bool generateCrystal(){
-		if (gridStatus.myStatus[x,z]==gridStatus.Status.Free) {
+		if (myStatus==Status.Free) {
 			crystal = (GameObject)Instantiate (crystal);
 			crystal.transform.position += transform.position;
 			gridStatus.myStatus[x,z]=gridStatus.Status.Crystal;
@@ -62,12 +68,24 @@ public class Square : MonoBehaviour {
 	}
 
 	public void generateEnemy(){
-		if (gridStatus.myStatus[x,z] == gridStatus.Status.Generator) {
+		if (myStatus == Status.Generator) {
 			enemyNormal = (GameObject)Instantiate (enemyNormal);
 			enemyNormal.transform.position = transform.position;
 		}
 	}
 
+	public void setGenerator(){
+		myStatus = Status.Generator;
+	}
+
+	public int setStatus(){
+		if (myStatus == Status.Crystal)
+			return 0;
+		else if (myStatus == Status.Free)
+			return 1;
+		else
+			return 2;
+	}
 
 	public void setX(int value){
 		x = value;
